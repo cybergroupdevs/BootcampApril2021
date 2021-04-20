df559f8f5383c896df4d470b6c379ece4cbd1c1b
Vikas Upadhayay <vikas.upadhayay@cybergindia.local>
2021-04-20 23:40:26 +0530

    ASP.Net project of making controller

---
+++ ASP.Net/NewWeb/NewWeb/Controllers/StudentController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+using NewWeb.Models;
+
+namespace NewWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        // GET: api/Student
+        [HttpGet]
+        public string TestAction()
+        {
+            return "this is a test get request";
+        }
+
+        // GET: api/Student/5
+        [Route("/Student/{id}")]
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<Student> GetById(int id)
+        {
+            StreamReader r = new StreamReader(@"~/resources/Students.json");
+            string json = r.ReadToEnd();
+            List<Student> items = JsonConvert.DeserializeObject<List<Student>>(json);
+            r.Close();
+            return items.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Student
+        [HttpPost]
+        public void Post([FromBody]Student value)
+        {
+            var Temp_Json = new Student();
+            Temp_Json.Id = value.Id;
+            Temp_Json.Name = value.Name;
+            Temp_Json.Department = value.Department;
+            Temp_Json.Age = value.Age;
+            Temp_Json.College = value.College;
+
+            StreamReader File = new StreamReader(@"c:\users\vikas.upadhayay\source\repos\NewWeb\NewWeb\resources\Students.json");
+            string Json = File.ReadToEnd();
+            List<Student> Items = JsonConvert.DeserializeObject<List<Student>>(Json);
+            Items.Add(Temp_Json);
+
+            File.Close();
+
+            string WriteJson = JsonConvert.SerializeObject(Items.ToArray());
+            StreamWriter WriteFile = new StreamWriter(@"c:\users\vikas.upadhayay\source\repos\NewWeb\NewWeb\resources\Students.json");
+            //write file logic here
+            WriteFile.Write(WriteJson);
+            WriteFile.Close();
+        }
+
+
+        // PUT: api/Student/5
+        [Route("/Student/{id}")]
+        [HttpPut("{id}")]
+        public ActionResult<string> Put(int id, [FromBody]Student value)
+        {
+            StreamReader r = new StreamReader(@"c:\users\vikas.upadhayay\source\repos\NewWeb\NewWeb\resources\Students.json");
+            string json = r.ReadToEnd();
+            List<Student> Items = JsonConvert.DeserializeObject<List<Student>>(json);
+
+            for (int i = 0; i < Items.Capacity; i++)
+            {
+                if (Items[i].Id == id)
+                {
+                    Items[i].Id = value.Id;
+                    Items[i].Name = value.Name;
+                    Items[i].Department = value.Department;
+                    Items[i].Age = value.Age;
+                    Items[i].College = value.College;
+                    break;
+                }
+            }
+            r.Close();
+            string WriteJson = JsonConvert.SerializeObject(Items.ToArray());
+            StreamWriter WriteFile = new StreamWriter(@"c:\users\vikas.upadhayay\source\repos\NewWeb\NewWeb\resources\Students.json");
+            WriteFile.Write(WriteJson);
+            WriteFile.Close();
+            return "Updated";
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public ActionResult<string> Delete(int id)
+        {
+            StreamReader File = new StreamReader(@"c:\users\vikas.upadhayay\source\repos\NewWeb\NewWeb\resources\Students.json");
+            string Json = File.ReadToEnd();
+            List<Student> Items = JsonConvert.DeserializeObject<List<Student>>(Json);
+
+            for (int i = 0; i < Items.Capacity; i++)
+            {
+                if (Items[i].Id == id)
+                {
+                    Items.RemoveAt(i);
+                    break;
+                }
+            }
+
+            File.Close();
+
+            string WriteJson = JsonConvert.SerializeObject(Items.ToArray());
+            StreamWriter WriteFile = new StreamWriter(@"c:\users\vikas.upadhayay\source\repos\NewWeb\NewWeb\resources\Students.json");
+            WriteFile.Write(WriteJson);
+            WriteFile.Close();
+            return "Record Deleted";
+        }
+    }
+}
