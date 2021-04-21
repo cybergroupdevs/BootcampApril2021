2600034f98bb62f619662f182ad515806c579bd0
Avneet Kaur <avneet.kaur@cybergindia.local>
2021-04-21 23:00:04 +0530

    web api assignment

---
+++ Assignment/Assignment/Models/StudentRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Models
+{
+    public class studentRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
