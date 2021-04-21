a2d7775c88c86b1cfa6bcec74cc2248e5a3bb7b6
Divyanshu Goyal <divyanshu.goyal@cybergindia.local>
2021-04-21 22:41:48 +0530

    Assignment 2 for ASP.net

---
+++ WebApplication4/WebApplication4/Models/StudentRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class StudentRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
