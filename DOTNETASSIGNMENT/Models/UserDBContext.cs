aa9f1b72f21a80701c57d406cf33ca5e9d820a2f
Bhavana Nagpal <bhavana.nagpal@cybergindia.local>
2021-04-22 01:10:57 +0530

    This is my 2nd  dotnet assignment

---
+++ DOTNETASSIGNMENT/Models/UserDBContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DOTNETASSIGNMENT.Models
+{
+    public partial class UserDBContext : DbContext
+    {
+        public UserDBContext()
+        {
+        }
+
+        public UserDBContext(DbContextOptions<UserDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=CYG306\\SQLEXPRESS;Database=UserDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.Property(e => e.Address)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
