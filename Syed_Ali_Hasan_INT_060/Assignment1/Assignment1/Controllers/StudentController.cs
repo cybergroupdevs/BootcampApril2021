b2a119590e18a73a29bd7b5b6d3d77f07f417357
Syed Ali Hasan <syed.hasan@cybergindia.local>
2021-04-21 15:28:40 +0530

    Completed Database Link

--- Syed_Ali_Hasan_INT_060/Assignment1/Assignment1/Controllers/StudentController.cs
+++ Syed_Ali_Hasan_INT_060/Assignment1/Assignment1/Controllers/StudentController.cs
@@ -4,113 +4,95 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Assignment.Models;
+using Assignment1.Models;
+using Assignment1.Models.Request;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace Assignment1.Controllers
 {
-    public static class Global
-    {
-        public static string getFile()
-        {
-            return @"E:\BootcampApril2021\Syed_Ali_Hasan_INT_060\Assignment1\Assignment1\Resource\Student.json";
-        }
-    }
     [Route("api/[controller]")]
     [ApiController]
     public class StudentController : ControllerBase
     {
+        StudentDBContext _context;
+        public StudentController(StudentDBContext context)
+        {
+            _context = context;
+        }
+
         // GET: api/Student
         [HttpGet]
-        public string testAction()
+        public ActionResult GetStudents()
         {
-            return "Test successful";
+            var studentList = _context.StudentInfo.ToList();
+            return Ok(studentList);
         }
 
-        // GET: api/Student/5
-        [HttpGet("{id}", Name = "Get")]
-        public ActionResult<Student> GetStudentById(int id)
-        {
-            StreamReader r = new StreamReader(Global.getFile());
-            string json = r.ReadToEnd();
-            List<Student> items = JsonConvert.DeserializeObject<List<Student>>(json);
-            var ans = items.Where(x => x.Id == id).FirstOrDefault();
-            return ans;
-        }
+        //// GET: api/Student/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public ActionResult<Student> GetStudentById(int id)
+        //{
+        //    StreamReader r = new StreamReader(Global.getFile());
+        //    string json = r.ReadToEnd();
+        //    List<Student> items = JsonConvert.DeserializeObject<List<Student>>(json);
+        //    var ans = items.Where(x => x.Id == id).FirstOrDefault();
+        //    return ans;
+        //}
 
-        // GET: api/Student/5
-        [HttpGet]
-        [Route("/api/Student/GetAllStudents")]
-        public ActionResult<List<Student>> GetAllStudents()
-        {
-            StreamReader r = new StreamReader(Global.getFile());
-            string json = r.ReadToEnd();
-            List<Student> items = JsonConvert.DeserializeObject<List<Student>>(json);
-            return items;
-        }
+        //// GET: api/Student/5
+        //[HttpGet]
+        //[Route("/api/Student/GetAllStudents")]
+        //public ActionResult<List<Student>> GetAllStudents()
+        //{
+        //    StreamReader r = new StreamReader(Global.getFile());
+        //    string json = r.ReadToEnd();
+        //    List<Student> items = JsonConvert.DeserializeObject<List<Student>>(json);
+        //    return items;
+        //}
 
         //POST: api/Student
         [HttpPost]
-        public void AddStudentInfo([FromBody]Student student)
+        public ActionResult AddStudent([FromBody]StudentInfo student)
         {
-            // Read existing json data
-            var jsonData = System.IO.File.ReadAllText(Global.getFile());
-            // De-serialize to object or create new list
-            var studentList = JsonConvert.DeserializeObject<List<Student>>(jsonData)
-                                  ?? new List<Student>();
-
-            studentList.Add(student);
-            jsonData = JsonConvert.SerializeObject(studentList);
-            System.IO.File.WriteAllText(Global.getFile(), jsonData);
+            _context.StudentInfo.Add(student);
+            _context.SaveChanges();
+            return Ok("Student Added successfully");
         }
 
         // PUT api/Student/5
         [HttpPut("{id}")]
-        public void UpdateStudentById(int id, [FromBody] Student student)
+        public ActionResult UpdateStudent(int id, [FromBody] StudentInfo studentRequest)
         {
-            // Read existing json data
-            var jsonData = System.IO.File.ReadAllText(Global.getFile());
-            // De-serialize to object or create new list
-            var studentList = JsonConvert.DeserializeObject<List<Student>>(jsonData)
-                                  ?? new List<Student>();
-
-            for (int i = 0; i < studentList.Capacity; i++)
-            {
-                if (studentList[i].Id == id)
-                {
-                    studentList[i].Id = student.Id;
-                    studentList[i].Name = student.Name;
-                    studentList[i].Department = student.Department;
-                    studentList[i].Age = student.Age;
-                    studentList[i].College = student.College;
-                    break;
-                }
-            }
-            jsonData = JsonConvert.SerializeObject(studentList);
-            System.IO.File.WriteAllText(Global.getFile(), jsonData);
+            StudentInfo studentInfo = _context.StudentInfo.FirstOrDefault(student => student.Id == id);
+            studentInfo.FirstName = studentRequest.FirstName;
+            studentInfo.LastName = studentRequest.LastName;
+            _context.StudentInfo.Update(studentInfo);
+            _context.SaveChanges();
+            return Ok(studentInfo);
         }
 
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void DeleteStudentById(int id)
-        {
-            // Read existing json data
-            var jsonData = System.IO.File.ReadAllText(Global.getFile());
-            // De-serialize to object or create new list
-            var studentList = JsonConvert.DeserializeObject<List<Student>>(jsonData)
-                                  ?? new List<Student>();
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void DeleteStudentById(int id)
+        //{
+        //    // Read existing json data
+        //    var jsonData = System.IO.File.ReadAllText(Global.getFile());
+        //    // De-serialize to object or create new list
+        //    var studentList = JsonConvert.DeserializeObject<List<Student>>(jsonData)
+        //                          ?? new List<Student>();
 
-            for (int i = 0; i < studentList.Capacity; i++)
-            {
-                if (studentList[i].Id == id)
-                {
-                    studentList.RemoveAt(i);
-                    break;
-                }
-            }
-            jsonData = JsonConvert.SerializeObject(studentList);
-            System.IO.File.WriteAllText(Global.getFile(), jsonData);
-        }
+        //    for (int i = 0; i < studentList.Capacity; i++)
+        //    {
+        //        if (studentList[i].Id == id)
+        //        {
+        //            studentList.RemoveAt(i);
+        //            break;
+        //        }
+        //    }
+        //    jsonData = JsonConvert.SerializeObject(studentList);
+        //    System.IO.File.WriteAllText(Global.getFile(), jsonData);
+        //}
     }
 }
