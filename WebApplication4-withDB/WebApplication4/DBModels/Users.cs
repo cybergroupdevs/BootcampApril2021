687eb899d0e89c275c1c1f50d11338570c7e2547
vsasvipul-cygrp <vipul.sinha@cygrp.com>
2021-04-23 23:56:29 +0530

    CRUD operations with DB

---
+++ WebApplication4-withDB/WebApplication4/DBModels/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication4.DBModels
+{
+    public partial class Users
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? AccId { get; set; }
+
+        public Account Acc { get; set; }
+    }
+}
