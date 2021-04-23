26a557d1c93eb9356933689fba9d68aa69ae1b3f
shreyaa1501 <82077002+shreyaa1501@users.noreply.github.com>
2021-04-23 23:54:53 +0530

    JWT token generated with hard core model

--- WebApplication4/Controllers/UserController.cs
+++ WebApplication4/Controllers/UserController.cs
@@ -5,6 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebApplication4.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebApplication4.Controllers
 {
@@ -12,6 +17,12 @@ namespace WebApplication4.Controllers
     [ApiController]
     public class UserController : Controller
     {
+        private IConfiguration _config;
+
+        public UserController(IConfiguration config)
+        {
+            _config = config;
+        }
         UserDBContext _context;
 
 
@@ -19,9 +30,51 @@ namespace WebApplication4.Controllers
         {
             _context = context;
         }
-
-
+        [AllowAnonymous]
         [HttpPost]
+        public IActionResult Login([FromBody]UserModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+
+            //Validate the User Credentials    
+            //Demo Purpose, I have Passed HardCoded User Information    
+            if (login.Username == "Jignesh")
+            {
+                user = new UserModel { Username = "Jignesh Trivedi", EmailAddress = "test.btest@gmail.com" };
+            }
+            return user;
+        }
+    
+
+[HttpPost]
         public ActionResult AddUser([FromBody] UserInfo user)
         {
             _context.UserInfo.Add(user);
