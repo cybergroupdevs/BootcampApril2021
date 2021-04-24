1ea971850f864520ca97742d2bc9af0babf5541e
vsasvipul-cygrp <vipul.sinha@cygrp.com>
2021-04-24 20:44:21 +0530

    CRUD Operations with GUI

---
+++ CRUD Operations with GUI/WebApplication5/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        UserDBContext _context;
+
+        public UserController(UserDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult AddUser([FromBody] UserInfo user)
+        {
+            _context.UserInfo.Add(user);
+            _context.SaveChanges();
+            return Ok("User Details Added successfully");
+        }
+
+        [HttpGet]
+        public ActionResult GetAllUsers()
+        {
+            var studentList = _context.UserInfo.ToList();
+            return Ok(studentList);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetById(int id)
+        {
+            UserInfo userInfo = _context.UserInfo.FirstOrDefault(user => user.Id == id);
+            return Ok(userInfo);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateUser(int id, [FromBody] UserInfo UserRequest)
+        {
+            UserInfo userInfo = _context.UserInfo.FirstOrDefault(user => user.Id == id);
+            userInfo.FirstName = UserRequest.FirstName;
+            userInfo.LastName = UserRequest.LastName;
+            userInfo.Age = UserRequest.Age;
+            userInfo.Address = UserRequest.Address;
+            _context.UserInfo.Update(userInfo);
+            _context.SaveChanges();
+            return Ok(userInfo);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteUser(int id)
+        {
+            var UserInfo = _context.UserInfo.FirstOrDefault(user => user.Id == id);
+            _context.UserInfo.Remove(UserInfo);
+            _context.SaveChanges();
+            return Ok(UserInfo);
+        }
+    }
+}
---
+++ CRUD Operations with GUI/WebApplication5/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WebApplication5.Models;
+
+namespace WebApplication5
+{
+    public class Startup
+    {
+        //readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<UserDBContext>(o => o.UseSqlServer("server =CYG466; database = UserDB; Trusted_Connection = True; "));
+            //services.AddCors(c => { c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin()); });
+            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+            //services.AddResponseCaching();
+            //services.AddControllers();
+            
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors(b => b.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+            app.UseMvc();
+
+        }
+    }
+}
