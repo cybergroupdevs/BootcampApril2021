9c899d642ac44d22b0a64fce60a77b6ab5439f9b
Ahana Drall <ahana.drall@cybergindia.local>
2021-04-26 07:54:34 +0530

    next

---
+++ candidate mngmt/repos/testProject/testProject/Controllers/QueryController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using testProject.Models;
+
+namespace testProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QueryController : ControllerBase
+    {
+
+        StudentdBContext _context;
+        public QueryController(StudentdBContext context)
+
+        {
+            _context = context;
+        }
+
+        // POST: api/Query
+        [HttpPost]
+        public ActionResult AddQuery([FromBody] QueryRequest queryRequest)
+        {
+            Query query = new Query()
+            {
+                Email = queryRequest.Email,
+                College = queryRequest.College,
+                
+                Issue = queryRequest.Issue
+
+            };
+
+            _context.Query.Add(query);
+            _context.SaveChanges();
+            return Ok("Query Added");
+        }
+
+       
+    }
+}
---
+++ candidate mngmt/repos/testProject/testProject/Models/Query.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testProject.Models
+{
+    public class Query
+    {
+        public string Email { get; set; }
+        public string College { get; set; }
+
+        public string Issue { get; set; }
+    }
+}
