46000d2f7996495c6033ffe969ce6af43ada8db9
nagpal-bhavana949 <82057650+nagpal-bhavana949@users.noreply.github.com>
2021-04-26 15:31:12 +0530

    Updated file

--- Colllege_ Management/login2.js
+++ Colllege_ Management/login2.js
@@ -1,39 +1,37 @@
 
-function logincheck(){​​​​​​​​
+function logincheck(){
     var TempName=document.getElementById("user");
     var TempPass=document.getElementById("pass");
     
-    varTempBook={​​​​​​​​
+    var TempBook={
     "UserName":TempName.value,
     "Password":TempPass.value
-    
-      }​​​​​​​​
+    }
     console.log(TempBook);
-    fetch("https://localhost:44376/api/USERS", {​​​​​​​​
+    fetch("https://localhost:44376/api/USERS",{
     method:"POST",
     mode:'cors', // no-cors, *cors, same-origin
     cache:'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials:'same-origin', // include, *same-origin, omit
-    headers: {​​​​​​​​
+    headers:{
     'Content-Type':'application/json'
     // 'Content-Type': 'application/x-www-form-urlencoded',
-      }​​​​​​​​,
+    },
     redirect:'follow', // manual, *follow, error
     referrerPolicy:'no-referrer',
     body:JSON.stringify(TempBook)
-    }​​​​​​​​)
+  })
     //.then(response => response.json())
     .then(response=>response.text())
-          .then((response) => {​​​​​​​​
-    console.log('token');
-    window.localStorage.setItem('token', response);
-    window.localStorage.getItem('token').toString();
+          .then((response) =>{
+    var token=response;
+    window.localStorage.setItem("token", token);
     openpage();
-          }​​​​​​​​);
+           });
     
-    }​​​​​​​​
+            }
+            function openpage(){
+              window.open("./info.html","_blank");
+            }
     
-    function openpage(){
-      window.open("./info.html","_blank");
-    }
     
